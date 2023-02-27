2513f38452a74973bf666f6be3f2e49dd2b78ce0
ldall03 <dallaireloic@gmail.com>
2023-02-27 12:02:56 -0500

    Removed printing in main

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ use crate::parser::Parser;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let args_concat: String = args[1..].join("");
-    println!("{}", args_concat);
     let tokens: Vec<Token> = tokenize(args_concat);
 
     let mut parser = Parser::new(tokens);
